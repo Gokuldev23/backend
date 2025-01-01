24388c3c76be0c56e5f20f138db2bfceaff84196
Gokuldev23 <gkforgeneral@gamil.com>
2025-01-01 22:53:40 +0530

    mongodb error handling has done

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -20,22 +20,47 @@ const createAContact = asyncHandler(async(req,res) =>{
     res.status(200).json({message:" contact created",data:contact})
 })
 
-const getSingleContact = asyncHandler((req,res) =>{
-    res.status(200).json({message:`You got contact ${req.params.id}`})
+const getSingleContact = asyncHandler(async(req,res) =>{
+
+    let id = req.params.id
+    if(!id){
+        return res.status(400).json({message:"id is not passed"})
+    }
+    const contact = await contactModel.findById(id)
+    if(!contact){
+        return res.status(401).json({message:"Contact not found"})
+    }
+    res.status(200).json({message:`You got contact ${req.params.id}`,data:contact})
 })
 
 
-const updateAContact = asyncHandler((req,res)=>{
+const updateAContact = asyncHandler(async(req,res)=>{
+    let id = req.params.id
+    if(!id){
+        return res.status(400).json({message:"id is not passed"})
+    }
     const {name,email} = req.body
     if(!name || !email){
         res.status(402).json({success:false,message:"Some fields are missing!"})
         return
     }
-    res.status(200).json({success:true,message:`Contact ${req.params.id} Successfully upadted`})
+    const contact = await contactModel.findByIdAndUpdate(
+        id,
+        req.body,
+        {new:true}
+    )
+    res.status(200).json({success:true,message:`Contact ${req.params.id} Successfully upadted`,data:contact})
 })
 
-const deleteAContact = asyncHandler((req,res)=>{
-    res.status(200).json({message:`the contact ${req.params.id} deleted`})
+const deleteAContact = asyncHandler(async(req,res)=>{
+    let id = req.params.id
+    if(!id){
+        return res.status(400).json({message:"id is not passed"})
+    }
+    let deleted = await contactModel.deleteOne({_id:id})
+    console.log({deleted})
+
+    return res.status(200).json({message:`the contact ${req.params.id} deleted`})
 })
 
 
