cc5b931934eecdc6559e455377beba408edcd796
Gokuldev23 <gkforgeneral@gamil.com>
2025-01-05 13:05:22 +0530

    jwt token added and signin using atoken added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,34 +1,68 @@
 const asyncHandler = require('express-async-handler')
-const contactModel = require('../models/contactModel')
+const User = require('../models/userModel')
+const bycrpt = require("bcrypt")
+const jwt = require('jsonwebtoken')
 
 
 const register = asyncHandler(async (req,res) =>{
     // let response = await contactModel.find()
-    res.status(200).json({message:"user register",data:null})
+    const {email,name,password} = req.body
+    if(!name || !email || !password){
+        return res.status(400).json({message:"Some fields are empty"})
+    }
+    let user = await User.findOne({email})
+    if(user){
+        return res.status(400).json({message:"User already exists"})
+    }else{
+        let hasedPassword = await bycrpt.hash(password,10)
+        let response = await User.create({
+            name,email,password:hasedPassword
+        })
+        res.status(200).json({message:"user register"})
+    }
 })
 
 const login = asyncHandler(async(req,res) =>{
-    const {name,email} = req.body
-    if(!name || !email){
+    const {password,email} = req.body
+    if(!password || !email){
         return res.status(400).json({message:"Some fields are empty"})
     }
-    // const contact = await contactModel.create({
-    //     name,
-    //     email
-    // })
-    res.status(200).json({message:"user logged In",data:null})
+
+    let user = await User.findOne({email}).lean()
+    let hashedPassword = user.password
+
+    let checkPassword = await bycrpt.compare(password,hashedPassword)
+    if(!checkPassword){
+        return res.status(400).json({message:"Password is incorrect"})
+    }
+
+    delete user.password
+    let a_token = jwt.sign(
+        {
+            userData:user
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn:'1m'}
+    )
+
+    res.status(200).json({message:"user logged In",data:a_token})
 })
 
 const currentUser = asyncHandler(async(req,res) =>{
 
-    // let id = req.params.id
-    // if(!id){
-    //     return res.status(400).json({message:"id is not passed"})
-    // }
-    // const contact = await contactModel.findById(id)
-    // if(!contact){
-    //     return res.status(401).json({message:"Contact not found"})
-    // }
+    const header = req.headers.authorization
+    if(!header){
+        res.status(401).json({message:"Unauthorised token not present or Invalid"})
+    }
+    let token = header.split(' ')[1]
+
+    let userData = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decode)=>{
+        if(err){
+            res.status(401).json({message:"User is Unauthorised"})
+        }
+        console.log(decode)
+    })
+    
     res.status(200).json({message:`Current User data`,data:null})
 })
 
